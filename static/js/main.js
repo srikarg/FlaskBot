0f2cb6d839a440eec8859aa3ea2eb4ba4d52e53d
Srikar Gudipati <srikarvgudipati@gmail.com>
2017-10-01 19:27:16 -0400

    Add initial version of front end

---
+++ static/js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+    var logMessage = function (message, user, log) {
+        log.append('<li><span class="user">' + user === 'flaskbot' ? 'Flaskbot' : 'You' + '</span><span class="message">' + message + '</span></li>');
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, '400');
+    }
+
+    var log = $('.log ul');
+    var input = $('.input');
+    var form = $('.form');
+    input.focus();
+
+    form.on('submit', function (event) {
+        event.preventDefault();
+
+        var text = input.val().trim();
+        input.val('');
+        logMessage(text, 'you', log);
+
+        if (text === '!clear') {
+            log.empty();
+            return;
+        } else if (text === '!hide') {
+            log.find('span.user').each(function () {
+                if ($(this).text().trim() === 'You') {
+                    $(this).parent('li').remove();
+                }
+            });
+            return;
+        }
+
+        if (text !== '') {
+            $.ajax({
+                type: 'POST',
+                url: '/',
+                data: {
+                    message: text
+                },
+                success: function (data) {
+                    logMessage(data.message, 'flaskbot', log);
+                    input.focus();
+                }
+            });
+        }
+    });
+});
+
